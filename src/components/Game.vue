65c99a44eec827467c7becd88856ac73f87bb1bf
Iana Spivac <yspivac@gmail.com>
2022-06-19 03:02:02 +0300

    Added main and radar

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="item">
     <div v-if="setted">
-      <div>Your role is: {{ role }}</div>
-      <div>Find the prey players</div>
-      <div class="map"></div>
-      <div>Longitude: {{ longitude }}, latitude: {{ latitude }}</div>
-      <div><router-link to="/main">Leave</router-link></div>
+      <div class="role">YOU ARE {{ role }}</div>
+      <div class="info">You have to remain alive</div>
+      <div class="time">{{ time }}</div>
+      <div class="info">to get 100 points</div>
+      <div class="map" :style="radarAngle">
+        <div v-for="player in players">
+          <div class="radar" :style="rotate(player.degree)">
+            <div class="point"></div>
+          </div>
+        </div>
+      </div>
     </div>
     <div v-else>
       <div v-if="loaded">
@@ -36,8 +42,12 @@ export default {
       longitude: null,
       errorStr: null,
       timer: null,
+      angle: 0,
+      direction: 0,
+      radarAngle: "rotate(0)deg",
     };
   },
+  
   computed: {
     role() {
       return "Hunter";
@@ -48,32 +58,48 @@ export default {
     loaded() {
       return true;
     },
+    time() {
+      return "3:48";
+    },
+    players() {
+      return [
+        {
+          id: "1",
+          degree: "0",
+        },
+        {
+          id: "2",
+          degree: "90",
+        },
+      ];
+    }
   },
+
+  created() {
+    document.addEventListener.call(window, "deviceorientation", (event) => {
+      this.radarAngle = `--radarAngle: rotate(${event.alpha}deg)`;
+    });
+  },
+
   mounted() {
-    this.$window.addEventListener('deviceorientation', this.showDirection(event));
     this.timer = setInterval(() => {
-      this.getLocation()
-    }, 10000)
+      this.getLocation();
+    }, 10000);
   },
+
   beforeDestroy() {
-    clearInterval(this.timer)
+    clearInterval(this.timer);
   },
+
   destroyed() {
-    this.window.removeEventListener('deviceorientation', this.showDirection());
+    window.removeEventListener("deviceorientation", this.showDirection());
   },
+
   methods: {
-    fetchInitialPosition() {
-      axios
-        .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        .then((response) => (this.info = response));
-    },
-    fetchStart() {
-      axios
-        .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-        .then((response) => (this.info = response));
-    },
-    showDirection(event) {
-      console.log(event.alpha)
+    rotate(degree) {
+      return {
+        "--rotation": `rotate(${degree}deg)`,
+      };
     },
     getLocation() {
       navigator.geolocation.getCurrentPosition(
@@ -88,8 +114,60 @@ export default {
           this.gettingLocation = false;
           this.errorStr = err.message;
         }
-      )
+      );
     },
   },
-}
+};
 </script>
+<style scoped>
+.radar {
+  background-color: transparent;
+  border-radius: 100%;
+  height: 200px;
+  width: 200px;
+  position: absolute;
+  transform: var(--rotation);
+  transition: transform 0.7s linear;
+  box-sizing: border-box;
+  margin-top: 30px;
+  margin-left: 22px;
+}
+.point {
+  height: 10px;
+  width: 10px;
+  background-color: red;
+  border-radius: 100%;
+  position: absolute;
+  top: -5px; /* -child size/2 */
+  left: 95px; /* parent size/2 - child size/2 */
+}
+.map {
+  height: 250px;
+  width: 250px;
+  position: relative;
+  transform: var(--radarAngle);
+  background-image: url("/images/radar.png");
+  background-size: 100%;
+  transition: transform 0.7s linear;
+  margin-left: auto;
+  margin-right: auto;
+}
+.time {
+  font-size: 30px;
+  border: 1px solid #00ff93;
+  width: max-content;
+  margin: 5px auto;
+  padding: 5px 35px;
+}
+.info {
+  font-size: 24px;
+}
+.role {
+  font-size: 32px;
+}
+.time,
+.info,
+.role {
+  text-align: center;
+}
+</style>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,10 +1,52 @@
 <template>
   <div class="item">
-    <div class="balance">
-    Your current points: {{points}}   
+    <div class="column">
+      <h1>PLAC3 H0LD3R</h1>
+    </div>
+    <div >
+      <div>
+        <div class="user underline">
+          <div class="avatar"></div>
+          <div>Us3rnam3</div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div>
+        <div class="info row balance underline">
+          <div>
+            <div>Balance:</div>
+          </div>
+          <div class="flex-bottom">
+            <div class="statistics-data">
+              {{ balance }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="statistics row underline">
+          <div class="flex-bottom">
+            <div class="statistics-data">
+              {{ victims }}
+            </div>
+          </div>
+          <div>
+            <div>caught victims</div>
+          </div>
+        </div>
+        <div class="victims statistics row underline">
+          <div>Time played as a victim</div>
+          <div class="statistics-data">
+            <div class="flex-bottom">
+              {{ time }}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="btn">
-      <router-link to="/game">Join game</router-link>
+      <RouterLink to="/game">ENTER THE GAME</RouterLink>
     </div>
   </div>
 </template>
@@ -12,13 +54,88 @@
 export default {
   data() {
     return {
-      count: 0
-    }
+      balance: "100",
+      victims: "4",
+      time: "4:20",
+      tag: "//01. <Home/>",
+      tag1: "//02. <game/>"
+    };
   },
   computed: {
     points() {
-      return 100
-    }
-  }
-}
+      return 100;
+    },
+  },
+};
 </script>
+<style scoped>
+.tags {
+  color: #4d4e53;
+  font-size: 10px;
+}
+.user {
+  font-size: 28px;
+}
+.balance,
+.statistics {
+  font-size: 18px;
+}
+
+.statistics-data {
+  border: 1px solid #00ff93;
+  border-bottom: none;
+  padding-left: 3px;
+  padding-right: 3px;
+}
+.row {
+  display: flex;
+  width: 50%;
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+.btn {
+  height: 200px;
+  width: 200px;
+  border-radius: 50%;
+  background-color: #00ff93;
+  font-size: 26px;
+  padding: 40px 60px;
+  cursor: pointer;
+  margin: 10px auto;
+}
+.btn a {
+  color: black;
+}
+
+.underline {
+  border-bottom: 1px solid #00ff93;
+}
+
+.column {
+  display: flex;
+  flex-direction: column;
+}
+
+.first-tags {
+  justify-content: space-between;
+}
+.row.balance,
+.row.victims {
+  margin-left: auto;
+}
+.victims .padding {
+  padding: 0 20px;
+}
+.row.balance,
+.statistics.row {
+  width: 200px;
+  display: flex;
+  justify-content: space-between;
+}
+.row.victims {
+  width: 250px;
+  display: flex;
+  justify-content: space-between;
+}
+
+</style>
